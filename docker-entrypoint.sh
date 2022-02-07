6792ba908f8e3b1de6039cdedaa052996b654bc8
masanchezp <miguelangel.sanchezprieto@telefonica.com>
2022-02-07 16:00:43 +0100

    first commit for app cloud mss training

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# dynamically get the container id into index.html
+echo "<p>Hello World from Cloud MSS Training demo</p>" > /usr/share/nginx/html/index.html
+echo "<p>This container was created on : $(date)</p>" >> /usr/share/nginx/html/index.html
+echo "<p>This container image was built using the automated Docker Hub build pipeline" >> /usr/share/nginx/html/index.html
+echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
+
+# start nginx
+nginx -g "daemon off;"
