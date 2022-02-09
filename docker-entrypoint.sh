68da92640d87da34af6a2ce7752112de2da70787
masanchezp <miguelangel.sanchezprieto@telefonica.com>
2022-02-09 17:40:36 +0100

    rsa and evil

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # dynamically get the container id into index.html
 echo "<p>Hello World from Cloud MSS Training demo</p>" > /usr/share/nginx/html/index.html
 echo "<p>This container was created on : $(date)</p>" >> /usr/share/nginx/html/index.html
-echo "<p>This container image was built using the automated Docker Hub build pipeline" >> /usr/share/nginx/html/index.html
+echo "<p>This container image was built using the automated Cloud MSS Training pipeline" >> /usr/share/nginx/html/index.html
 echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
 
 # start nginx
