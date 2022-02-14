387e1843fd0d60bf166d0fd79ecba9b6a2114071
masanchezp <miguelangel.sanchezprieto@telefonica.com>
2022-02-14 20:18:45 +0100

    V2-web

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,18 +6,6 @@
 # echo "<p>This container image was built using the automated Cloud MSS Training pipeline - Scan only alerts - AWS EB desde jenkins" >> /usr/share/nginx/html/index.html
 # echo "<p>This request was served from container id : $(hostname)</p>" >> /usr/share/nginx/html/index.html
 
-#echo "<!DOCTYPE html>" > /usr/share/nginx/html/index.html
-#echo "<html>" >> /usr/share/nginx/html/index.html
-#echo "<head>" >> /usr/share/nginx/html/index.html
-#echo "<title>DevOps Essentials Sample App</title>" >> /usr/share/nginx/html/index.html
-#echo "</head>" >> /usr/share/nginx/html/index.html 
-#echo "<body style=&amp;quot;background-color: #1A729D;&amp;quot;>" >> /usr/share/nginx/html/index.html
-#echo "<h1 style=&amp;quot;font-family: Courier; color: #434342; font-size: 48px; text-align: center;&amp;quot;>" >> /usr/share/nginx/html/index.html
-#echo "DevOps is TTECH!" >> /usr/share/nginx/html/index.html
-#echo "</h1>" >> /usr/share/nginx/html/index.html
-#echo "</body>" >> /usr/share/nginx/html/index.html
-#echo "</html>" >> /usr/share/nginx/html/index.html
-
 cat <<EOF > /usr/share/nginx/html/index.html
 <!DOCTYPE html>
 <html>
@@ -26,7 +14,7 @@ cat <<EOF > /usr/share/nginx/html/index.html
 </head>
 <body style="background-color: #1A729D;">
 <h1 style="font-family: Courier; color: #434342; font-size: 48px; text-align: center;">
-Web de pruebas Demo V1!
+Web de pruebas Demo V2!
 </h1>
 </body>
 </html>
