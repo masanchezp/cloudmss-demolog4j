664defe95a547da696428e38332481322ea3c49a
masanchezp <miguelangel.sanchezprieto@telefonica.com>
2022-02-18 10:15:25 +0100

    V1 training

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ cat <<EOF > /usr/share/nginx/html/index.html
 </head>
 <body style="background-color: #1A729D;">
 <h1 style="font-family: Courier; color: #434342; font-size: 48px; text-align: center;">
-Web de pruebas Demo v1 Training jueves tarde se cuela el virus!
+Web de pruebas Demo v1 Training!
 </h1>
 </body>
 </html>
