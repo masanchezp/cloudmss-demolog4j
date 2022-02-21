671fb63b1ca758160935e0477564c8349aca1118
masanchezp <miguelangel.sanchezprieto@telefonica.com>
2022-02-21 09:31:02 +0100

    v1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ cat <<EOF > /usr/share/nginx/html/index.html
 </head>
 <body style="background-color: #1A729D;">
 <h1 style="font-family: Courier; color: #434342; font-size: 48px; text-align: center;">
-Web de pruebas Demo v1 Training desde Ubuntu local!
+Web de pruebas Demo v1 para Training desde Ubuntu local!
 </h1>
 </body>
 </html>
